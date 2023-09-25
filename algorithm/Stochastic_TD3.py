afd8c3b0821cb1ec6e16df426608df086b710f66
david <davidvalenciaredrovan@gmail.com>
2023-09-25 13:39:56 +1300

    testing for two critics

--- algorithm/Stochastic_TD3.py
+++ algorithm/Stochastic_TD3.py
@@ -20,11 +20,14 @@ class STC_TD3(object):
                  action_num,
                  device):
 
-        self.actor_net  = Actor(observation_size=observation_size, action_num = action_num).to(device)
-        self.critic_net = Critic(observation_size=observation_size, action_num = action_num).to(device)
+        self.actor_net        = Actor(observation_size=observation_size, action_num = action_num).to(device)
+        self.target_actor_net = copy.deepcopy(self.actor_net).to(device)
 
-        self.target_actor_net  = copy.deepcopy(self.actor_net).to(device)
+        self.critic_net        = Critic(observation_size=observation_size, action_num = action_num).to(device) # todo try to this as ensemble
         self.target_critic_net = copy.deepcopy(self.critic_net).to(device)
+
+        self.critic_net_two        = Critic(observation_size=observation_size, action_num = action_num).to(device)
+        self.target_critic_net_two = copy.deepcopy(self.critic_net_two).to(device)
 
         self.gamma = 0.99
         self.tau   = 0.005
@@ -38,7 +41,9 @@ class STC_TD3(object):
         lr_actor   = 1e-4
         lr_critic  = 1e-3
         self.actor_net_optimiser  = torch.optim.Adam(self.actor_net.parameters(),   lr=lr_actor)
-        self.critic_net_optimiser = torch.optim.Adam(self.critic_net.parameters(),  lr=lr_critic)
+
+        self.critic_net_optimiser     = torch.optim.Adam(self.critic_net.parameters(),     lr=lr_critic)
+        self.critic_net_two_optimiser = torch.optim.Adam(self.critic_net_two.parameters(), lr=lr_critic)
 
 
     def select_action_from_policy(self, state, evaluation=False, noise_scale=0.1):
@@ -76,39 +81,67 @@ class STC_TD3(object):
 
         with torch.no_grad():
             next_actions = self.target_actor_net(next_states)
-            target_noise = 0.2 * torch.randn_like(next_actions)
-            target_noise = torch.clamp(target_noise, min=-0.5, max=0.5)
+            target_noise = 0.2 * torch.randn_like(next_actions)  # TODO what about this value 0.2 can be changed?
+            target_noise = torch.clamp(target_noise, min=-0.5, max=0.5) # TODO same here
             next_actions = next_actions + target_noise
             next_actions = torch.clamp(next_actions, min=-1, max=1)
 
-            u , std = self.target_critic_net(next_states, next_actions)
-            u_target   =  rewards +  self.gamma * (1 - dones) * u
-            std_target =  self.gamma * std
+            u , std         = self.target_critic_net(next_states, next_actions)
+            u_two , std_two = self.target_critic_net_two(next_states, next_actions)
+
+            # -------- Key part here -------------- #
+            # average the distribution to create a unique distribution, encapsulating the whole outputs
+            # this is not a mixture of gaussian mixture
+            u_aver   = (u + u_two) / 2
+            std_aver = (std + std_two) / 2
+
+            u_target   =  rewards +  self.gamma * u_aver * (1 - dones)
+            std_target =  self.gamma * std_aver
+
+        #     # with  this did not work
+        #     # u_target   = rewards + self.gamma * (1 - dones) * u
+        #     # std_target = self.gamma * std * (1 - dones) + 1e-8
+
             target_distribution = torch.distributions.normal.Normal(u_target, std_target)
 
         u_current, std_current = self.critic_net(states, actions)
-        current_distribution = torch.distributions.normal.Normal(u_current, std_current)
+        current_distribution   = torch.distributions.normal.Normal(u_current, std_current)
+
+        u_current_two, std_current_two = self.critic_net_two(states, actions)
+        current_distribution_two       = torch.distributions.normal.Normal(u_current_two, std_current_two)
 
         # Compute critic loss
-        critic_loss = torch.distributions.kl_divergence(current_distribution, target_distribution).mean()
+        critic_loss     = torch.distributions.kl_divergence(current_distribution, target_distribution).mean()  # todo try other divergence too
+        critic_loss_two = torch.distributions.kl_divergence(current_distribution_two, target_distribution).mean()
 
         # Update the Critic
         self.critic_net_optimiser.zero_grad()
         critic_loss.backward()
         self.critic_net_optimiser.step()
 
+        self.critic_net_two_optimiser.zero_grad()
+        critic_loss_two.backward()
+        self.critic_net_two_optimiser.step()
 
-        if self.learn_counter % self.policy_update_freq == 0:
-
+        if self.learn_counter % self.policy_update_freq == 0:  # todo try if i change the freq update
             # Update Actor
-            actor_q_u, actor_q_std = self.critic_net(states, self.actor_net(states))
-            actor_loss = -actor_q_u.mean()
+            actor_q_u, actor_q_std         = self.critic_net(states, self.actor_net(states))
+            actor_q_u_two, actor_q_std_two = self.critic_net_two(states, self.actor_net(states))
+
+            actor_q_u_aver   = (actor_q_u + actor_q_u_two) / 2
+            actor_q_std_aver = (actor_q_std + actor_q_std_two) / 2
+
+            #actor_distribution = torch.distributions.normal.Normal(actor_q_u_aver, actor_q_std_aver)  # todo what can i do with this?
+            actor_loss = -actor_q_u_aver.mean()
 
             self.actor_net_optimiser.zero_grad()
             actor_loss.backward()
             self.actor_net_optimiser.step()
 
             for target_param, param in zip(self.target_critic_net.parameters(), self.critic_net.parameters()):
+                target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+
+            for target_param, param in zip(self.target_critic_net_two.parameters(), self.critic_net_two.parameters()):
                 target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
 
             for target_param, param in zip(self.target_actor_net.parameters(), self.actor_net.parameters()):
